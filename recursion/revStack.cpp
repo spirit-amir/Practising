caffed68688012136fea7896e4aed56f583f30d2
Amir Aashique Khan <khanaashiqueamir@gmail.com>
2024-08-25 10:41:36 +0530

    Add modified files after reset

--- recursion/revStack.cpp
+++ recursion/revStack.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 void fun(int i, int arr[], int n)
 {
-    if(i>=n/2) 
+    if(i>=n/2)
      return;
     swap(arr[i],arr[n-i-1]);
     fun(i+1, arr, n);
@@ -26,7 +26,7 @@ int main()
  for(int i=0; i<n; i++)
    cin>>arr[i];
 
-fun(0,arr,n);
-for( int i = 0; i<n;i++) cout<< arr[i] << " ";
-return 0;
+ fun(0,arr,n);
+ for( int i = 0; i<n;i++) cout<< arr[i] << " ";
+ return 0;
 }
